9dcbe6e8318629eb43c08f0b11a694713336f8fc
pipinokyo <cuneyt.omer@gmail.com>
2025-05-06 17:47:48 -0400

    homework3 vpc

--- terreform-homework/homework3/homework3.tf
+++ terreform-homework/homework3/homework3.tf
@@ -1,4 +1,4 @@
-# Create VPC
+# Terraform script to create a VPC with public and private subnets, route tables, and NAT gateway
 resource "aws_vpc" "app_vpc1" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -6,7 +6,9 @@ resource "aws_vpc" "app_vpc1" {
   }
 }
 
-# Create Internet Gateway
+# create Internet Gateway
+# The Internet Gateway allows communication between instances in your VPC and the internet.
+# It is a horizontally scaled, redundant, and highly available VPC component that allows communication between instances in your VPC and the internet.
 resource "aws_internet_gateway" "app_igw1" {
   vpc_id = aws_vpc.app_vpc1.id
   tags = {
@@ -14,7 +16,9 @@ resource "aws_internet_gateway" "app_igw1" {
   }
 }
 
-# Create Public Subnets
+# create Public Subnets
+# Public subnets are subnets that have a route to the internet through an internet gateway.
+# Instances in public subnets can communicate directly with the internet.
 resource "aws_subnet" "app_pub_subnet_1a" {
   vpc_id            = aws_vpc.app_vpc1.id
   cidr_block        = "10.0.1.0/24"
@@ -43,6 +47,8 @@ resource "aws_subnet" "app_pub_subnet_3c" {
 }
 
 # Create Private Subnets
+# Private subnets are subnets that do not have a route to the internet through an internet gateway.
+# Instances in private subnets cannot communicate directly with the internet.
 resource "aws_subnet" "app_priv_subnet_1a" {
   vpc_id            = aws_vpc.app_vpc1.id
   cidr_block        = "10.0.11.0/24"
@@ -71,6 +77,9 @@ resource "aws_subnet" "app_priv_subnet_3c" {
 }
 
 # Create Route Tables
+# Route tables are used to control the routing of traffic within your VPC.
+# Each subnet must be associated with a route table.
+# A route table contains a set of rules, called routes, that are used to determine where network traffic is directed.
 resource "aws_route_table" "app_public_rt1" {
   vpc_id = aws_vpc.app_vpc1.id
   tags = {
@@ -85,7 +94,9 @@ resource "aws_route_table" "app_private_rt1" {
   }
 }
 
-# Create Public Route
+# Create Public Route to Internet Gateway
+# The route table for the public subnets must have a route to the internet gateway.
+# This allows instances in the public subnets to communicate with the internet.
 resource "aws_route" "public_internet_gateway" {
   route_table_id         = aws_route_table.app_public_rt1.id
   destination_cidr_block = "0.0.0.0/0"
@@ -93,6 +104,8 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 # Associate Public Subnets with Public Route Table
+# The public subnets must be associated with the public route table.
+# This allows instances in the public subnets to use the route table to route traffic to the internet.
 resource "aws_route_table_association" "public_subnet_1a" {
   subnet_id      = aws_subnet.app_pub_subnet_1a.id
   route_table_id = aws_route_table.app_public_rt1.id
@@ -109,6 +122,8 @@ resource "aws_route_table_association" "public_subnet_3c" {
 }
 
 # Associate Private Subnets with Private Route Table
+# The private subnets must be associated with the private route table.
+# This allows instances in the private subnets to use the route table to route traffic to the NAT gateway.
 resource "aws_route_table_association" "private_subnet_1a" {
   subnet_id      = aws_subnet.app_priv_subnet_1a.id
   route_table_id = aws_route_table.app_private_rt1.id
@@ -125,11 +140,15 @@ resource "aws_route_table_association" "private_subnet_3c" {
 }
 
 # Allocate Elastic IP for NAT Gateway
+# An Elastic IP address is a static, public IPv4 address designed for dynamic cloud computing.
+# An Elastic IP address is associated with your AWS account and can be associated with any instance or network interface in your VPC.
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 }
 
 # Create NAT Gateway
+# A NAT gateway is a managed NAT service that provides network address translation (NAT) for instances in a private subnet.
+# A NAT gateway allows instances in a private subnet to initiate outbound traffic to the internet while preventing unsolicited inbound traffic from the internet.
 resource "aws_nat_gateway" "app_natgateway1" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.app_pub_subnet_1a.id
@@ -139,6 +158,8 @@ resource "aws_nat_gateway" "app_natgateway1" {
 }
 
 # Create Private Route to NAT Gateway
+# The route table for the private subnets must have a route to the NAT gateway.
+# This allows instances in the private subnets to communicate with the internet through the NAT gateway.
 resource "aws_route" "private_nat_gateway" {
   route_table_id         = aws_route_table.app_private_rt1.id
   destination_cidr_block = "0.0.0.0/0"
