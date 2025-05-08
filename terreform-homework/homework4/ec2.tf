71802bca16027226020f3db22e791eb58dda04d0
pipinokyo <cuneyt.omer@gmail.com>
2025-05-08 18:19:50 -0400

    homework4

--- terreform-homework/homework4/ec2.tf
+++ terreform-homework/homework4/ec2.tf
@@ -1,22 +1,17 @@
-resource "aws_instance" "main" {
-  ami                       = data.aws_ami.amazon_linux_2023.id
-  instance_type             = var.instance_type
-  subnet_id                 = aws_subnet.app_pub_subnet[0].id
-  tags = {
-    Name                    = "${var.env}-instance"
-    Environment = var.env  
-    }
-  vpc_security_group_ids = [aws_security_group.main.id]
-  user_data = templatefile("userdata.sh", {
-    environment = var.env
-  })
-}
-
 resource "aws_security_group" "main" {
   name        = "${var.env}-security-group"
   description = "allow SSH and HTTP"
-  
+  vpc_id      = aws_vpc.app_vpc1.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "${var.env}-security-group"
+  }
 }
+
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   count             = length(var.ingress_ports)
   security_group_id = aws_security_group.main.id
@@ -29,5 +24,17 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.main.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
+}
+
+resource "aws_instance" "main" {
+  ami                       = data.aws_ami.amazon_linux_2023.id
+  instance_type             = var.instance_type
+  subnet_id                 = aws_subnet.app_pub_subnet[0].id
+  vpc_security_group_ids    = [aws_security_group.main.id]
+  
+  tags = {
+    Name        = "${var.env}-instance"
+    Environment = var.env
+  }
 }
--- terreform-homework/homework4/outputs.tf
+++ terreform-homework/homework4/outputs.tf
@@ -1,5 +1,34 @@
-output "aws_instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.main[*].public_ip
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.app_vpc1.id
+}
 
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.app_pub_subnet[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.app_priv_subnet[*].id
+}
+
+output "ec2_instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.main.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.main.id
+}
+
+output "security_group_id" {
+  description = "ID of the security group attached to the EC2 instance"
+  value       = aws_security_group.main.id
+}
+
+output "nat_gateway_ip" {
+  description = "Public IP of the NAT Gateway"
+  value       = aws_eip.nat_eip.public_ip
 }
