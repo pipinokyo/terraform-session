f6916d4046bb569a3b105c12d8b334945746f3d1
pipinokyo <cuneyt.omer@gmail.com>
2025-05-10 13:25:47 -0400

    homework5

--- terreform-homework/homework5/data_sources.tf
+++ terreform-homework/homework5/data_sources.tf
@@ -1,24 +1,24 @@
 // Fetch amazon linux 2023 ami id  
 
-data "aws_ami" "amazon_linux_2023" {
-  most_recent      = true
-  owners           = ["amazon"]
+ data "aws_ami" "amazon_linux_2023" {
+   most_recent      = true
+   owners           = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.7.*"]
-  }
+   filter {
+     name   = "name"
+     values = ["al2023-ami-2023.7.*"]
+   }
 
-  filter {
-    name    = "architecture"
-    values  = ["x86_64"]
-  }
+   filter {
+     name    = "architecture"
+     values  = ["x86_64"]
+   }
 
-  filter {
-    name    = "virtualization-type"
-    values  = ["hvm"]
-  }
-}
+   filter {
+     name    = "virtualization-type"
+     values  = ["hvm"]
+   }
+ }
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
--- terreform-homework/homework5/outputs.tf
+++ terreform-homework/homework5/outputs.tf
@@ -1,59 +1,59 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.app_vpc1.id
-}
+# output "vpc_id" {
+#   description = "The ID of the VPC"
+#   value       = aws_vpc.app_vpc1.id
+# }
 
-output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = aws_subnet.app_pub_subnet[*].id
-}
+# output "public_subnet_ids" {
+#   description = "List of public subnet IDs"
+#   value       = aws_subnet.app_pub_subnet[*].id
+# }
 
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = aws_subnet.app_priv_subnet[*].id
-}
+# output "private_subnet_ids" {
+#   description = "List of private subnet IDs"
+#   value       = aws_subnet.app_priv_subnet[*].id
+# }
 
-output "ec2_instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = try(aws_instance.main.public_ip, "Not assigned")
-  depends_on  = [aws_instance.main]
-}
-output "ec2_instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.main.id
-}
+# output "ec2_instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = try(aws_instance.main.public_ip, "Not assigned")
+#   depends_on  = [aws_instance.main]
+# }
+# output "ec2_instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.main.id
+# }
 
-output "security_group_id" {
-  description = "ID of the security group attached to the EC2 instance"
-  value       = aws_security_group.main.id
-}
+# output "security_group_id" {
+#   description = "ID of the security group attached to the EC2 instance"
+#   value       = aws_security_group.main.id
+# }
 
-output "nat_gateway_ip" {
-  description = "Public IP of the NAT Gateway"
-  value       = aws_eip.nat_eip.public_ip
-}
+# output "nat_gateway_ip" {
+#   description = "Public IP of the NAT Gateway"
+#   value       = aws_eip.nat_eip.public_ip
+# }
 
-output "application_url" {
-  description = "URL to access the web application"
-  value       = try("http://${aws_instance.main.public_ip}", "Instance has no public IP")
-}
+# output "application_url" {
+#   description = "URL to access the web application"
+#   value       = try("http://${aws_instance.main.public_ip}", "Instance has no public IP")
+# }
 
-output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.app_alb.dns_name
-}
+# output "alb_dns_name" {
+#   description = "The DNS name of the ALB"
+#   value       = aws_lb.app_alb.dns_name
+# }
 
-output "target_group_arn" {
-  description = "ARN of the target group"
-  value       = aws_lb_target_group.app_tg.arn
-}
+# output "target_group_arn" {
+#   description = "ARN of the target group"
+#   value       = aws_lb_target_group.app_tg.arn
+# }
 
-output "launch_template_id" {
-  description = "ID of the launch template"
-  value       = aws_launch_template.app_lt.id
-}
+# output "launch_template_id" {
+#   description = "ID of the launch template"
+#   value       = aws_launch_template.app_lt.id
+# }
 
-output "asg_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.app_asg.name
-}
+# output "asg_name" {
+#   description = "Name of the Auto Scaling Group"
+#   value       = aws_autoscaling_group.app_asg.name
+# }
