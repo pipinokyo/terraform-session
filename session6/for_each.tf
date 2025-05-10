09d8acaa70bb6bc61247e1d8ab7df4b43c836004
pipinokyo <cuneyt.omer@gmail.com>
2025-05-10 16:29:19 -0400

    session6

--- session6/for_each.tf
+++ session6/for_each.tf
@@ -1,4 +1,10 @@
+# resource "aws_sqs_queue" "for_each_queue" {
+#     for_each = var.for_each_names
+#     name     = each.value
+   
+# }
+
 resource "aws_sqs_queue" "for_each_queue" {
-    for_each = var.for_each_names
-    name     = each.value
+    for_each = toset(local.queue_names)
+    name     = each.key
 }
---
+++ session6/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  queue_names = [for i in range(1, 3) : "queue-${i}"]
+}
