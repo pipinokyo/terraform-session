169187cc4d941c720c1a3c38aa42c52f9e5d4b3e
pipinokyo <cuneyt.omer@gmail.com>
2025-05-13 21:52:56 -0400

    terraform modules

--- session7/dev/main.tf
+++ session7/dev/main.tf
@@ -38,3 +38,30 @@ data "aws_ami" "amazon_linux_2023" {
     values  = ["hvm"]
   }
 }
+
+module "erkin" {
+  source = "github.com/Ekanomics/terraform-session//modules/sg"
+  name   = "erkin"
+  description = "Security group for erkin instance"
+  ingress_ports = [22]
+  ingress_cidr = [ "10.0.0.0/32" ]
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
