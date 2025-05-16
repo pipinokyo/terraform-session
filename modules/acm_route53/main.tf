beab188cf967b1f3ca2338c51a7c500829dc8710
pipinokyo <cuneyt.omer@gmail.com>
2025-05-16 14:36:06 -0400

    terraform homework 7

--- modules/acm_route53/main.tf
+++ modules/acm_route53/main.tf
@@ -18,14 +18,14 @@ resource "aws_route53_record" "root" {
   }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = "www.${var.domain_name}"
+#   type    = "A"
 
-  alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = var.alb_dns_name
+#     zone_id                = var.alb_zone_id
+#     evaluate_target_health = true
+#   }
+# }
--- modules/acm_route53/outputs.tf
+++ modules/acm_route53/outputs.tf
@@ -1,9 +1,9 @@
-output "certificate_arn" {
-  description = "ARN of the ACM certificate"
-  value       = data.aws_acm_certificate.issued.arn
-}
+# output "certificate_arn" {
+#   description = "ARN of the ACM certificate"
+#   value       = data.aws_acm_certificate.issued.arn
+# }
 
-output "hosted_zone_id" {
-  description = "ID of the Route53 hosted zone"
-  value       = aws_route53_zone.primary.zone_id
-}
+# output "hosted_zone_id" {
+#   description = "ID of the Route53 hosted zone"
+#   value       = aws_route53_zone.primary.zone_id
+# }
